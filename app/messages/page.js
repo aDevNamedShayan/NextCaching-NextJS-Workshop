a80275723445155c0bb3d067c28898c45ff8b3c7
Shayan <nwryshayan500@gmail.com>
2025-03-18 21:50:53 +0330

    005 Understanding The Data Cache & Cache Settings

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,12 @@
+import { unstable_noStore } from 'next/cache';
 import Messages from '@/components/messages';
 
+// export const revalidate = 5 //seconds
+// export const dynamic = 'force-dynamic' //cachimg options. force-dynamic = no-store, force-static = force-cache //is file wide
+
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      revalidate: 5
-    }
-  });
+  // unstable_noStore() //same are 'force-dynamic' and 'no-store' //is component / function wide
+  const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
