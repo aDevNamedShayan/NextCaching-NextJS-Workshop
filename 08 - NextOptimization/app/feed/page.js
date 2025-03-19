dd474d46409a4c11d1c3ad1debb4fd27be75f129
Shayan <nwryshayan500@gmail.com>
2025-03-19 20:37:30 +0330

    011 Configuring Dynamic Page Metadata

--- 08 - NextOptimization/app/feed/page.js
+++ 08 - NextOptimization/app/feed/page.js
@@ -1,6 +1,20 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'All Posts',
+//   description: 'Browse all our posts.',
+// }
+
+export async function generateMetadata(data) {
+  const posts = await getPosts();
+  const numOfPosts = posts.length
+  return {
+    title: `Browse ${numOfPosts} of our car posts!`,
+    description: 'Browse our car posts!',
+  }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
